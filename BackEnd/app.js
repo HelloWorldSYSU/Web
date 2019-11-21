eb325b2a36ae3da7ada2dc929fb424de55e3f7a3
unknown <git@gitee.com:MaHuSYSU/MOSAD_HW4.git>
2019-11-22 05:44:40 +0800

    version 1.0.1

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,10 +1,17 @@
 var http = require('http');
+var fs = require('fs')
+var path = require('path')
+
+var h5path = path.resolve(__dirname, '..');
+h5path += '\\FrontEnd\\panda\\panda.html'
+
+
+console.log(h5path);
 
 var server = http.createServer(function (request, response) {
 	console.log("Request received");
-	response.writeHead(200, { 'Content-Type': 'text/plain'});
-	response.write('Hello world');
-	response.end();
+	response.writeHead(200, { 'Content-Type': 'text/html'});
+	fs.createReadStream(h5path, 'utf8').pipe(response);
 });
 
 server.listen(80, "0.0.0.0");
